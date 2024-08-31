40584b13e53983b35005e9484ab1d7bc10dbaa56
Ammarsaqib-programmer <150575040+Ammarsaqib-programmer@users.noreply.github.com>
2024-08-31 00:16:57 -0700

    Create command_types.sql

---
+++ command_types.sql
@@ -0,0 +1,7 @@
+Types of Command in SQL
+•	DDL (Data Definition Language) : create, alter, rename
+•	DQL (Data Query Language) : select
+•	DML (Data Manipulation Language ) : insert, update, delete
+•	DCL (Data Control Language) : grant & revoke 
+•	TCL (Transaction Control Language) : start transaction
+
