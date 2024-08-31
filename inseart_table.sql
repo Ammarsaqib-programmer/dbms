adb8dd95993c07df8fe0958549ecc8fd774dee95
Ammarsaqib-programmer <150575040+Ammarsaqib-programmer@users.noreply.github.com>
2024-08-31 00:14:38 -0700

    Create inseart_table.sql

---
+++ inseart_table.sql
@@ -0,0 +1,10 @@
+USE university;
+
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT NOT NULL
+);
+
+INSERT INTO student VALUES (1, 'ALI', 26);
+INSERT INTO student VALUES (2, 'MUZAMILL', 20);
